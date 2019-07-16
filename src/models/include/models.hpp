b063f66031cba52bec72be7319c9ab1adcb9f5a5
Wade Foster <jwade109@vt.edu>
2019-07-16 00:12:21 +0000

    Add models package

---
+++ src/models/include/models.hpp
@@ -0,0 +1,11 @@
+#ifndef RVT_MODEL_HPP
+#define RVT_MODEL_HPP
+
+namespace rvt
+{
+
+void foo(int);
+
+} // namespace rvt
+
+#endif 
---
+++ src/models/src/models.cpp
@@ -0,0 +1,15 @@
+// model.cpp
+// just an example source file for this package
+
+#include "models.hpp"
+
+namespace rvt
+{
+
+void foo(int)
+{
+    // do nothing
+}
+
+} // namespace rvt
+
