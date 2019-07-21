2c2b85c90dbe3ffd121421e988cdf6301862184d
Wade Foster <jwade109@vt.edu>
2019-07-21 04:39:11 +0000

    Add pin parameters for drivers

--- src/hardware/src/motor_driver.cpp
+++ src/hardware/src/motor_driver.cpp
@@ -1,6 +1,7 @@
 // motor_driver.cpp
 
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 #include <hardware/MotorCommand.h>
 
 void recieve_motor_command(const hardware::MotorCommand &mc)
@@ -11,11 +12,21 @@ void recieve_motor_command(const hardware::MotorCommand &mc)
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "motor_driver");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
 
-    ROS_INFO("Starting motor driver");
+    int positive_pin, negative_pin;
+    if (!nh.getParam("positive", positive_pin) ||
+        !nh.getParam("negative", negative_pin))
+    {
+        ROS_FATAL("Failed to get motor pin mappings!");
+        ros::shutdown();
+    }
+    ROS_INFO("Starting motor driver on pins %d, %d",
+        positive_pin, negative_pin);
 
     auto sub = nh.subscribe("motor_cmd", 100, recieve_motor_command);
 
     ros::spin();
+
+    return 0;
 }
--- src/hardware/src/solenoid_driver.cpp
+++ src/hardware/src/solenoid_driver.cpp
@@ -11,11 +11,20 @@ void recieve_solenoid_command(const hardware::SolenoidCommand &sc)
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "solenoid_driver");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
 
-    ROS_INFO("Starting solenoid driver");
+    int positive_pin, negative_pin;
+    if (!nh.getParam("positive", positive_pin) ||
+        !nh.getParam("negative", negative_pin))
+    {
+        ROS_FATAL("Failed to get solenoid pin mappings!");
+        ros::shutdown();
+    }
+    ROS_INFO("Starting solenoid driver on pins %d, %d",
+        positive_pin, negative_pin);
 
-    auto sub = nh.subscribe("solenoid_cmd", 100, recieve_solenoid_command);
+    auto sub = nh.subscribe("solenoid_cmd", 100,
+        recieve_solenoid_command);
 
     ros::spin();
 }
