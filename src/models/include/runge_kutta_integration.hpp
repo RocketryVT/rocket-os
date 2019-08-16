e8fe0070dc0e8bcdb3259de713ed201e1091be11
Wade Foster <jwade109@vt.edu>
2019-08-16 11:04:06 -0400

    Runge-kutta rk4 works now!

--- src/models/include/runge_kutta_integration.hpp
+++ src/models/include/runge_kutta_integration.hpp
@@ -37,7 +37,7 @@ const runge_kutta_tableau<2> midpoint_method(
     // C vector
    {0, 0.5});
 
-const runge_kutta_tableau<3> default_rk3(
+const runge_kutta_tableau<3> rk3(
     // A matrix
    {0,   0, 0,
     0.5, 0, 0,
@@ -47,16 +47,29 @@ const runge_kutta_tableau<3> default_rk3(
     // C vector
    {0, 0.5, 1});
 
-const runge_kutta_tableau<4> default_rk4(
+const runge_kutta_tableau<4> rk4(
     // A matrix
    {0,   0,   0, 0,
     0.5, 0,   0, 0,
     0,   0.5, 0, 0,
     0,   0,   1, 0},
     // b vector
-   {1.0/6, 1.0/3, 1.0/3, 1.0/6},
+   {1.0/6, 2.0/6, 2.0/6, 1.0/6},
     // c vector
-   {0, 0.5, 0.5, 0});
+   {0, 0.5, 0.5, 1});
+
+const runge_kutta_tableau<6> rk45(
+    // A matrix
+   {0,            0,           0,           0,            0,       0,
+    1.0/4,        0,           0,           0,            0,       0,
+    3.0/32,       9.0/32,      0,           0,            0,       0,
+    1932.0/2197, -7200.0/2197, 7296.0/2197, 0,            0,       0,
+    439.0/216,   -8,           3680.0/513, -845.0/4104,   0,       0,
+   -8.0/27,       2,          -3544.0/2565, 1859.0/4104, -11.0/40, 0},
+    // B vector
+   {16.0/135, 0, 6656.0/12825, 28561.0/56430, -9.0/50, 2.0/55},
+    // C vector
+   {0, 1.0/4, 3.0/8, 12.0/13, 1, 1.0/2});
 
 const runge_kutta_tableau<7> dormand_prince(
     // A matrix
@@ -72,9 +85,8 @@ const runge_kutta_tableau<7> dormand_prince(
     // C vector
    {0, 0.2, 0.3, 0.8, 8.0/9, 1, 1});
 
-
 template <size_t M, size_t N>
-std::array<lambda::vector<M>, N> compute_k_coefficients(
+lambda::vector<M> estimate_next_state(
     double t, double timestep,
     const lambda::vector<M> y,
     const ordinary_diff_eq<M> &f,
@@ -82,19 +94,24 @@ std::array<lambda::vector<M>, N> compute_k_coefficients(
 {
     std::array<lambda::vector<M>, N> k;
 
-    k[0] = f(t, y);
+    lambda::vector<M> ret = y;
 
     for (size_t i = 0; i < N; ++i)
     {
         lambda::vector<M> sum;
         for (size_t j = 0; j < i; ++j)
         {
-            sum += rkt.a[i][j]*k[j];
+            sum = sum + rkt.a.at(i, j)*k.at(j);
         }
-        k[i] = f(t + rkt.c[2]*timestep, y + timestep*sum);
+
+        lambda::column_vector<M> prev_k;
+        if (i > 0) prev_k = k.at(i - 1);
+
+        k[i] = timestep*f(t + rkt.c.at(i)*timestep, y + prev_k*rkt.c.at(i));
+        ret = ret + k[i]*rkt.b.at(i, 0);
     }
 
-    return k;
+    return ret;
 }
 
 
@@ -122,7 +139,7 @@ std::array<lambda::vector<M>, N> compute_k_coefficients(
  * ydotkhist    - Ny x Nt double matrix
  *                Time derivative values at each integration step
  *
- * @author: Matt Marti
+ * @author: Matt Marti<
  * @date: 2019-04-29
  */
 
@@ -130,64 +147,27 @@ std::array<lambda::vector<M>, N> compute_k_coefficients(
 
 template <size_t M, size_t N>
 std::tuple<std::vector<lambda::column_vector<M>>,
-           std::vector<double>,
-           std::vector<lambda::column_vector<M>>>
+           std::vector<double>>
 runge_kutta_integration(ordinary_diff_eq<M> &ydot,
     const lambda::column_vector<M> &y0,
     double t_initial, double t_final,
     double timestep,
-    const runge_kutta_tableau<N> &tableau = default_rk4)
+    const runge_kutta_tableau<N> &tableau)
 {
-    std::vector<lambda::column_vector<M>> y_history, ydot_history;
-
     std::vector<double> time_history =
         lambda::range(t_initial, t_final, timestep);
 
-    const size_t ny = N;
     const size_t nt = time_history.size();
-
-    y_history.resize(nt);
-    ydot_history.resize(nt);
+    std::vector<lambda::column_vector<M>> y_history(nt);
     y_history[0] = y0;
 
-    // Runge-Kutta integration loop
-    for (size_t k = 1; k < nt; ++k)
+    for (size_t i = 1; i < nt; ++i)
     {
-        std::cout << ydot_history[k] << std::endl;
-
-        // Initialize Runge Kutta step
-        std::vector<lambda::vector<M>> Kjhist;
-        Kjhist.reserve(ny);
-        lambda::vector<M> current_y = y_history[k-1];
-        double current_time = time_history[k];
-
-        // Runge-Kutta loop formulation
-        Kjhist.push_back(ydot(current_time, current_y));
-        for (size_t j = 1; j < N; ++j)
-        {
-            double targ = current_time + timestep*tableau.c[j];
-            lambda::vector<M> karg = current_y;
-            for (size_t i = 0; i < j; ++i)
-                karg = karg + timestep*tableau.a(j,i)*Kjhist[i];
-            Kjhist[j] = ydot(targ, karg);
-        }
-
-        // Final compute state at time tk
-        lambda::matrix<M, N> kjhist_mat;
-        for (size_t col = 0; col < N; ++col)
-            for (size_t row = 0; row < M; ++row)
-                kjhist_mat(row, col) = Kjhist[col][row];
-
-        y_history[k] = current_y + timestep*kjhist_mat*tableau.b;
-
-        // Save ydot for ouptut
-        ydot_history[k] = Kjhist[0];
+        y_history.at(i) = estimate_next_state(time_history.at(i - 1),
+            timestep, y_history.at(i - 1), ydot, tableau);
     }
 
-    // Save ydot for ouptut
-    ydot_history[nt-1] = ydot(t_final, y_history[nt-1]);
-
-    return std::make_tuple(y_history, time_history, ydot_history);
+    return std::make_tuple(y_history, time_history);
 }
 
 } // namespace rvt
