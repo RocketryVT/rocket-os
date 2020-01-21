931afa4575d22d966aac1fcaaf6c05496ce2426f
Gimli Beaglebone <jwade109@vt.edu>
2020-01-20 21:18:00 -0500

    Add ctrl-c handler to LED demo script

--- pythonLED/simple-LED-output.py
+++ pythonLED/simple-LED-output.py
@@ -1,16 +1,21 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
+import signal
+
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 GPIO.cleanup()
-counter = 0;
 GPIO.setup("P9_11", GPIO.OUT)
 
 while True:
-    time.sleep(1)
     GPIO.output("P9_11", GPIO.HIGH)
     print("HIGH ")
     time.sleep(1)
     GPIO.output("P9_11", GPIO.LOW)
     print("LOW ")
+    time.sleep(1)
 
-
-    
