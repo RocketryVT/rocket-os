f98d060900dda4c3a053605ab4322d57e1cd7e68
Gimli Beaglebone <jwade109@vt.edu>
2020-02-22 20:59:44 -0500

    Improve pin unit testing reusability

--- pythonLED/simple-LED-output.py
+++ pythonLED/simple-LED-output.py
@@ -1,6 +1,7 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 import signal
+import sys
 
 def signal_handler(sig, frame):
     GPIO.cleanup()
@@ -8,7 +9,22 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-pins = ["P9_15", "P9_13"]
+if len(sys.argv) < 2:
+    print("usage:   python simple-led-output.py <pins>")
+    print("example: python simple-led-output.py P9_13 P9_15")
+    exit()
+
+pins = sys.argv[1:]
+print("Testing pins " + ", ".join(pins))
+
+P9 = [11, 12, 13, 15, 17, 18, 23, 24, 26, 27, 30, 41]
+P8 = [7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 26]
+all_pins = ["P9_{}".format(x) for x in P9] + ["P8_{}".format(x) for x in P8]
+
+for pin in pins:
+    if pin not in all_pins:
+        print("Error: " + pin + " is not a valid GPIO pin!")
+        exit()
 
 GPIO.cleanup()
 for pin in pins:
