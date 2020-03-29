852243b18871a2ab1ce2c85ccae6c1f29637dff2
Gimli Beaglebone <jwade109@vt.edu>
2020-03-28 23:58:59 -0400

    Add SensorReading.msg to sensor drivers
    This will break nodes which depend on the old sensor message types

--- src/sensors/scripts/pressure_transducer_driver.py
+++ src/sensors/scripts/pressure_transducer_driver.py
@@ -4,7 +4,7 @@
 
 import Adafruit_BBIO.ADC as adc
 import rospy
-from std_msgs.msg import Float32
+from sensors.msg import SensorReading
 import sys
 
 def voltage_to_pressure(voltage):
@@ -15,34 +15,49 @@ def voltage_to_pressure(voltage):
 
 def read_and_publish(event):
 
+    global sequence_number
+
     voltage = adc.read(adc_pin)*1.8
     pressure = voltage_to_pressure(voltage)
-    pub_volt.publish(voltage)
-    pub_press.publish(pressure)
+    msg = SensorReading()
+
+    msg.header.seq = sequence_number
+    sequence_number += 1
+    msg.header.stamp = rospy.Time.now()
+
+    msg.voltage = voltage
+    msg.reading = pressure
+    msg.unit = "psig"
+
+    publisher.publish(msg)
 
 
-adc.setup()
-rospy.init_node("pressure_transducer_driver")
+if __name__ == "__main__":
 
-name = rospy.get_name()
-try:
-    adc_pin = rospy.get_param(name + "/pin")
-    period = rospy.get_param(name + "/period")
-except:
-    rospy.logerr("Failed to retrieve configuration from rosparam server.")
-    rospy.signal_shutdown("Unavailable config.")
-    exit()
+    sequence_number = 0
 
-all_pins = ["AIN{}".format(x) for x in range(0,7)]
-if adc_pin not in all_pins:
-    rospy.logerr("Provided pin " + adc_pin + " not a valid ADC pin (" + str(all_pins) + ")")
-    exit()
+    adc.setup()
+    rospy.init_node("pressure_transducer_driver")
 
-pub_press = rospy.Publisher(name + "/pressure", Float32, queue_size=10);
-pub_volt = rospy.Publisher(name + "/voltage", Float32, queue_size=10);
-rospy.Timer(rospy.Duration(period), read_and_publish)
+    name = rospy.get_name()
+    try:
+        adc_pin = rospy.get_param(name + "/pin")
+        period = rospy.get_param(name + "/period")
+    except:
+        rospy.logerr("Failed to retrieve configuration from rosparam server.")
+        rospy.signal_shutdown("Unavailable config.")
+        exit()
 
-rospy.loginfo(("Starting pressure transducer driver on ADC {} " +
-               "and polling period {} seconds.").format(adc_pin, period))
-rospy.spin()
+    all_pins = ["AIN{}".format(x) for x in range(0,7)]
+    if adc_pin not in all_pins:
+        rospy.logerr("Provided pin " + adc_pin + \
+            " not a valid ADC pin (" + str(all_pins) + ")")
+        exit()
+
+    publisher = rospy.Publisher(name, SensorReading, queue_size=10);
+    rospy.Timer(rospy.Duration(period), read_and_publish)
+
+    rospy.loginfo(("Starting pressure transducer driver on ADC {} " +
+                   "and polling period {} seconds.").format(adc_pin, period))
+    rospy.spin()
 
