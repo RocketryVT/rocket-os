cd9f3d0103e8acd3d738d7e3b7d5a440f21844c4
Wade Foster <jwade109@vt.edu>
2020-04-07 02:41:13 -0400

    Integrate fill_admin with new message types

--- src/admin/scripts/fill_admin.py
+++ src/admin/scripts/fill_admin.py
@@ -3,13 +3,26 @@
 import rospy
 from std_msgs.msg import String, Float32, UInt8
 from sensors.msg import SensorReading
+from hardware.msg import DriverCommand
+
+
+def on_shutdown():
+
+    dc = DriverCommand()
+    dc.header.stamp = rospy.Time.now()
+    dc.source = name
+    dc.priority = admin_priority
+    dc.command = dc.RELEASE
+    solenoid_cmd.publish(dc)
+
+    rospy.loginfo("Exiting.")
 
 
 def toggle_fill(state):
 
     global fill_ongoing
     fill_ongoing = state
-    solenoid_cmd.publish(state)
+    set_solenoid(state)
 
 
 def print_relevant_data(event):
@@ -50,10 +63,21 @@ def get_los(message):
 
 def get_readiness(message):
 
-    readiness = message.data
-    if readiness != 2 and fill_ongoing:
-        rospy.logwarn("Fill only enabled for readiness level 2.")
-        toggle_fill(False)
+    global current_readiness
+    current_readiness = message.data
+
+
+def set_solenoid(state):
+
+    dc = DriverCommand()
+    dc.header.stamp = rospy.Time.now()
+    dc.source = name
+    dc.priority = admin_priority
+    if state:
+        dc.command = dc.SOLENOID_ACTIVE
+    else:
+        dc.command = dc.SOLENOID_INACTIVE
+    solenoid_cmd.publish(dc)
 
 
 def get_command(message):
@@ -62,6 +86,13 @@ def get_command(message):
     command = message.data
 
     if command == "begin fill":
+        if current_readiness is None:
+            rospy.logwarn("Haven't received readiness update -- " + \
+                "unsafe to continue.")
+            return
+        elif current_readiness != 2:
+            rospy.logwarn("Fill only enabled for readiness level 2.")
+            return
         rospy.logwarn("Beginning nitrous oxide fill.")
         toggle_fill(True)       
  
@@ -76,20 +107,25 @@ if __name__ == "__main__":
     last_pressure = None
     fill_ongoing = False
     max_safe_pressure = 900
+    admin_priority = 4
+    current_readiness = None
 
-    rospy.init_node("fill_admin")
+    rospy.init_node("fill_admin", log_level=rospy.DEBUG)
+    name = rospy.get_name()
 
     rospy.Subscriber("/commands", String, get_command)
     rospy.Subscriber("/los", Float32, get_los)
     rospy.Subscriber("/readiness_level", UInt8, get_readiness)
     rospy.Subscriber("/sensors/float_switch", SensorReading, get_lls_reading)
     rospy.Subscriber("/sensors/ox_tank_transducer", SensorReading, get_pressure)
-    solenoid_cmd = rospy.Publisher("/hardware/solenoid/request", UInt8, queue_size=10)
+    solenoid_cmd = rospy.Publisher("/hardware/solenoid", DriverCommand, queue_size=10)
 
     rospy.sleep(1)
 
-    rospy.loginfo("Assuming primary control of the solenoid.")
-    solenoid_cmd.publish(0)
+    rospy.loginfo("Assuming control of the solenoid.")
+    set_solenoid(False)
+
+    rospy.on_shutdown(on_shutdown)
 
     rospy.Timer(rospy.Duration(3), print_relevant_data)
 
