5ec043ba378aebd4f8da987b4750d545c9791dbe
jazzjackson0 <jazz@vt.edu>
2022-01-09 15:01:54 -0500

    Added Publisher for 'tank full' state

--- src/admin/scripts/fill_admin.py
+++ src/admin/scripts/fill_admin.py
@@ -7,18 +7,12 @@ Fill Admin: Manages the vehicle filling process
 			Available Actions: 
 				+ Start & Stop vehicle fill process
 				+ Set state of Solenoid valve
-				
-			Data Managed:
-				+ Pressure Data
-				+ Liquid Level Data
-				+ Loss of Signal Data
-				+ Readiness Level
 
 			Num of Functions: 9
 '''
 
 import rospy
-from std_msgs.msg import String, Float32, UInt8
+from std_msgs.msg import String, Float32, UInt8, Bool
 from sensors.msg import SensorReading
 from hardware.msg import DriverCommand
 
@@ -97,10 +91,13 @@ def get_lls_reading(message):
 	
     global last_lls
     global fill_ongoing
+    global full
     last_lls = message.reading
 
     if message.reading and fill_ongoing:
         rospy.logwarn("Liquid level switch tripped -- stopping fill!")
+        full = True
+        tank_state.publish(full)
         toggle_fill(False)
 
 
@@ -187,6 +184,7 @@ if __name__ == "__main__":
     max_safe_pressure = 900
     admin_priority = 4
     current_readiness = None
+    full = False
 
 	#Initialize Node
     rospy.init_node("fill_admin", log_level=rospy.DEBUG)
@@ -201,6 +199,7 @@ if __name__ == "__main__":
 	
 	#Set Publisher
     solenoid_cmd = rospy.Publisher("/hardware/solenoid", DriverCommand, queue_size=10)
+    tank_state = rospy.Publisher("tank_state", Bool, queue_size=10)
 
     rospy.sleep(1)
 
@@ -209,7 +208,7 @@ if __name__ == "__main__":
 
     rospy.on_shutdown(on_shutdown)
 
-	#Frequency that messages are published to the Topic
+	#Set message Publishing Frequency
     rospy.Timer(rospy.Duration(3), print_relevant_data)
 
     rospy.spin()
