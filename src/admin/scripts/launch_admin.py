fec91927d3eaba3dd9e67776cd0225041bc348a7
Binay Rijal <binaypr@vt.edu>
2022-03-27 15:28:07 -0400

    Changed shebangs again

--- src/admin/scripts/launch_admin.py
+++ src/admin/scripts/launch_admin.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 '''
 Launch Admin: Manages the Launch Countdown and Launch of the vehicle.
--- src/webserver/scripts/sensorServer/SensorServer.py
+++ src/webserver/scripts/sensorServer/SensorServer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 from HttpServer import BaseHTTPRequestHandler, HTTPServer
